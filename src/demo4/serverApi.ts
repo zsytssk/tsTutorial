12e3ff2e1afc86af4dc9e68437489fea9ddcc418
张世阳 <ZHANGSHIYANG162@pingan.com.cn>
2019-07-08 16:32:32 +0800

    master:[update]

---
+++ src/demo4/serverApi.ts
@@ -0,0 +1,8 @@
+type CreateRoleItemData = {};
+
+type RoleData = CreateRoleItemData & {
+    is_create?: boolean;
+    is_cur?: boolean;
+};
+
+//  http://tst-sgglory-h5-lizhi-stg216.1768.com/ roleList
